2436059ccbb84adae398f9911849968f31ee93c3
unclezs <1585503310@qq.com>
2020-12-01 19:56:00 +0800

    log samples

---
+++ samples-shiro/shiro-local/src/test/java/com/unclezs/samples/shiro/LoginAndOutTest.java
@@ -0,0 +1,28 @@
+package com.unclezs.samples.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.env.BasicIniEnvironment;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @author zhanghongguo@sensorsdata.cn
+ * @since 2020/12/01 16:22
+ */
+public class LoginAndOutTest {
+  @Test
+  public void loginTest() {
+    BasicIniEnvironment environment = new BasicIniEnvironment("classpath:shiro.ini");
+    SecurityManager securityManager = environment.getSecurityManager();
+    SecurityUtils.setSecurityManager(securityManager);
+    Subject subject = SecurityUtils.getSubject();
+    subject.login(new UsernamePasswordToken("uncle", "123"));
+    Assert.assertTrue(subject.isAuthenticated());
+    subject.logout();
+  }
+}
