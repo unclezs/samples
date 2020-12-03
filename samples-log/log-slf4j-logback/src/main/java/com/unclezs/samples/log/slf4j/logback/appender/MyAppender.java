a6a8ab3077a8798eb6af14278777c4fe157c641a
unclezs <1585503310@qq.com>
2020-12-03 20:02:12 +0800

    add logback samples

---
+++ samples-log/log-slf4j-logback/src/main/java/com/unclezs/samples/log/slf4j/logback/appender/MyAppender.java
@@ -0,0 +1,58 @@
+package com.unclezs.samples.log.slf4j.logback.appender;
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+
+/**
+ * @author zhanghongguo@sensorsdata.cn
+ * @since 2020/12/03 15:02
+ */
+public class MyAppender extends AppenderBase<ILoggingEvent> {
+
+  static int MAX_COUNT = 10;
+  int counter = 0;
+  int limit = MAX_COUNT;
+
+  PatternLayoutEncoder encoder;
+
+  @Override
+  public void start() {
+    if (this.encoder == null) {
+      addError("No encoder set for the appender named [" + name + "].");
+      return;
+    }
+    super.start();
+  }
+
+  @Override
+  public void append(ILoggingEvent event) {
+    if (counter >= limit) {
+      return;
+    }
+    //格式化
+    String bytes = this.encoder.getLayout().doLayout(event);
+    System.out.print(bytes);
+    counter++;
+  }
+
+  /**
+   * 通过getter setter设置
+   */
+  public PatternLayoutEncoder getEncoder() {
+    return encoder;
+  }
+
+  public void setEncoder(PatternLayoutEncoder encoder) {
+    this.encoder = encoder;
+  }
+
+  public void setLimit(int limit) {
+    this.limit = limit;
+  }
+
+  public int getLimit() {
+    return limit;
+  }
+}
---
+++ samples-log/log-slf4j-logback/src/main/java/com/unclezs/samples/log/slf4j/logback/appender/MyAppenderSample.java
@@ -0,0 +1,17 @@
+package com.unclezs.samples.log.slf4j.logback.appender;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhanghongguo@sensorsdata.cn
+ * @since 2020/12/03 19:37
+ */
+public class MyAppenderSample {
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(MyAppenderSample.class);
+    for (int i = 0; i < 20; i++) {
+      logger.info("第{}次", i);
+    }
+  }
+}
