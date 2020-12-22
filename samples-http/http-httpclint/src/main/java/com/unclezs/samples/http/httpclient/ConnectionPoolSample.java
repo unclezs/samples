1b7caf556c7719c05824322ea508e99c06483a0b
Unclezs <1585503310@qq.com>
2020-12-22 21:20:32 +0800

    [update]document

---
+++ samples-http/http-httpclint/src/main/java/com/unclezs/samples/http/httpclient/ConnectionPoolSample.java
@@ -0,0 +1,11 @@
+package com.unclezs.samples.http.httpclient;
+
+/**
+ * @author blog.unclezs.com
+ * @date 2020/12/20 5:32 下午
+ */
+public class ConnectionPoolSample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ samples-http/http-httpclint/src/main/java/com/unclezs/samples/http/httpclient/HttpSample.java
@@ -0,0 +1,22 @@
+package com.unclezs.samples.http.httpclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.fluent.Content;
+import org.apache.hc.client5.http.fluent.Form;
+import org.apache.hc.client5.http.fluent.Request;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * @author blog.unclezs.com
+ * @date 2020/12/20 5:26 下午
+ */
+@Slf4j
+public class HttpSample {
+    public static void main(String[] args) throws IOException {
+        Content content = Request.get("https://www.bxwxorg.com/read/87912/")
+                .execute().returnContent();
+        System.out.println(content.asString(Charset.defaultCharset()));
+    }
+}
