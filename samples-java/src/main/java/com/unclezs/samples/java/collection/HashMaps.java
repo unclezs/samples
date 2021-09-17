b72e4fa355c11391baaeee6087146502139cbf90
zhanghongguo <zhanghongguo@sensorsdata.cn>
2021-09-17 16:16:15 +0800

    feat: java samples

---
+++ samples-java/src/main/java/com/unclezs/samples/java/collection/HashMaps.java
@@ -0,0 +1,13 @@
+package com.unclezs.samples.java.collection;
+
+import java.util.HashMap;
+
+/**
+ * @author blog.unclezs.com
+ * @since 2021/9/17 16:10
+ */
+public class HashMaps {
+  public static void main(String[] args) {
+    new HashMap<>(0).put(2, 3);
+  }
+}
