d5acbc53e6674ae99d8b4ca8b627ae0fa3f165a8
unclezs <unclezs@qq.com>
2022-07-06 08:10:33 +0800

    update

---
+++ simples-spring/springboot-actuator/src/main/java/com/unclezs/springboot/actuator/App.java
@@ -0,0 +1,15 @@
+package com.unclezs.springboot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author blog.unclezs.com
+ * @since 2022/7/6 7:55 AM
+ */
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
